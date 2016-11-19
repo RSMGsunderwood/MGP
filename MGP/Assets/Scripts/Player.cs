d8560ef7d4515eba490133ab9e44923260c84ff4
Stephen Underwood <sunderwood@robotseamonstergames.com>
2016-11-18 17:48:27 -0800

    Adjustments and gitignore

---
+++ MGP/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public string playerName;
+	public enum PlayerNumber{
+		Player1,
+		Player2,
+		Player3,
+		Player4
+	};
+	public PlayerNumber playerNumber;
+}
