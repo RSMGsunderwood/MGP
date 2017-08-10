bdb65ffa01e539dfc5a74906ef9dffc14a9a00ef
Stephen Underwood <sunderwood@robotseamonstergames.com>
2017-08-10 13:20:26 -0700

    Spammin Cannon progress.

---
+++ MGP/Assets/Scripts/Minigame/SpamminCannon/MinigameSpamminCannon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinigameSpamminCannon : MinigameMain {
+
+	public Color rCol, bCol, yCol, gCol;
+	public Image displayButton;
+	public SpamminCannonPlayerHud[] playerHuds;
+	int currentChoice;
+
+	//Initialize game on startup
+	void Awake(){
+		InputHandler.ButtonPressed += this.ButtonPress;
+	}
+
+	//When this screen is destroyed we need to unsubscribe
+	void OnDestroy(){
+		InputHandler.ButtonPressed -= this.ButtonPress;
+	}
+
+	IEnumerator ShowNewButton(){
+		float lerp = 0;
+		for (float i = 0; i < .2f; i += Time.deltaTime) {
+			lerp = Mathf.SmoothStep (1, .8f, (.2f - i) / .2f);
+
+			yield return null;
+		}
+	}
+
+	public void ChooseRandomButton(){
+		currentChoice = Random.Range (1, 5);
+	}
+
+	//Button input override
+	public override void ButtonPress(int player, InputHandler.Buttons button){
+		if (button == InputHandler.Buttons.y) {
+			
+		}
+		if (button == InputHandler.Buttons.b) {
+			
+		}
+		if (button == InputHandler.Buttons.x) {
+			
+		}
+		if (button == InputHandler.Buttons.a) {
+			
+		}
+	}
+}
---
+++ MGP/Assets/Scripts/Minigame/SpamminCannon/SpamminCannonPlayerHud.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpamminCannonPlayerHud : MonoBehaviour {
+
+	public Image gaugeFill;
+	public RectTransform cannonTran;
+	float currentPower;
+
+	void Update(){
+		if (GameHandler.instance.timer > 0) {
+			currentPower -= Time.deltaTime;
+			Mathf.Clamp01 (currentPower);
+			gaugeFill.fillAmount = currentPower;
+		}
+	}
+
+	public void AddToGauge(int player){
+		currentPower += (1 - currentPower) * .1f;
+		Mathf.Clamp01 (currentPower);
+		GameHandler.instance.players [player].pointScore++;
+		StopCoroutine ("CannonShake");
+		StartCoroutine ("CannonShake");
+	}
+
+	IEnumerator CannonShake(){
+		float temp = 0;
+		for (float i = 0; i < .2f; i += Time.deltaTime) {
+			temp = Mathf.Sin (Time.time * 10f) * (20f*(.2f-i));
+			cannonTran.localEulerAngles = new Vector3 (0, 0, temp);
+			yield return null;
+		}
+		cannonTran.localEulerAngles = Vector3.zero;
+	}
+}
